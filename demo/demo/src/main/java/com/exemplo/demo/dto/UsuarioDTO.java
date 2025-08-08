996d86f06f58973660ab9d836ab8291dc1044718
Ryan Torres <ryantorres77690@gmail.com>
2025-08-08 17:08:03 -0300

    demo banco de dados integrado

---
+++ demo/demo/src/main/java/com/exemplo/demo/dto/UsuarioDTO.java
@@ -0,0 +1,11 @@
+package com.exemplo.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioDTO {
+    private String nome;
+    private String email;
+}
---
+++ demo/demo/src/main/java/com/exemplo/demo/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.exemplo.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private boolean ativo = true;
+}
