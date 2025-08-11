52cedb4c3fde04536f88afa0c9514bfa909b3302
Ryan Torres <ryantorres77690@gmail.com>
2025-08-11 11:27:24 -0300

    Stream

--- JavaExercicios/JavaPratica/src/StringElements.java
+++ JavaExercicios/JavaPratica/src/StringElements.java
@@ -3,6 +3,8 @@ import java.util.StringTokenizer;
 public class StringElements {
     public static void main(String[] args) {
         String[] elements = {"foo", "bar","teste"};
+        String singleString = String.join("+",elements);
+        System.out.println(singleString);
 
     }
 }
