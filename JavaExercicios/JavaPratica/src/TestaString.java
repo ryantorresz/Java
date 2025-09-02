9f049521fc40b9e76148db1f83fd8c217020eb86
Ryan Torres <ryantorres77690@gmail.com>
2025-09-02 15:36:34 -0300

    Armazenando Strings

---
+++ JavaExercicios/JavaPratica/src/TestaString.java
@@ -0,0 +1,10 @@
+public class TestaString {
+    public static void main(String[] args) {
+        String valor1 = "alura";
+        String valor2 = "alura";
+        System.out.println(valor1 == valor2);
+
+        String valor3 = new String("alura");
+        System.out.println(valor1 == valor3);
+    }
+}
