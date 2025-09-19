477fbf5d94fd1ed7c7dd0496230349a2a4658e87
Ryan Torres <ryantorres77690@gmail.com>
2025-09-19 20:39:57 -0300

    Refatorando projeto

--- Kafta_Spring_Alura/pix-consumidor/src/main/java/com/alura/pix/exception/KeyNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.alura.pix.exception;
-
-public class KeyNotFoundException extends RuntimeException {
-}
--- Kafta_Spring_Alura/pix-produtor/src/main/java/com/alura/pix/dto/PixStatus.java
+++
@@ -1,5 +0,0 @@
-package com.alura.pix.dto;
-
-public enum PixStatus {
-    EM_PROCESSAMENTO, PROCESSADO, ERRO
-}
