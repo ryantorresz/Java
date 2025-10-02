70fe4b999b5ae984ae4be7ece24078e12f06928c
Ryan Torres <ryantorres77690@gmail.com>
2025-10-02 08:15:29 -0300

    gerenciando sessões

--- seguranca-java-3/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ seguranca-java-3/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -2,6 +2,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -39,6 +40,10 @@ public class ConfiguracoesSeguranca {
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
                         .permitAll())
+                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
+                        .alwaysRemember(true)
+                )
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 }
